5758ee0a535e357cfd17b61b436a3f7447d5960b
Nikolai Kolotilov <x08042@gamail.com>
2022-04-19 18:32:19 +0500

    Исправлены ошибки

--- html/order.php
+++ html/order.php
@@ -1,48 +1,58 @@
 <?php
-require "../src/config.php";
-require "../src/class.db.php";
-require "../src/class.burger.php";
-
-/*Задача #3.2
-Скрипт должен:
-Проверить, существует ли уже пользователь с таким email, если нет - создать его, если да -
-    увеличить число заказов по этому email. Двух пользователей с одинаковым email быть не может.
-Сохранить данные заказа - id пользователя, сделавшего заказ, дату заказа, полный адрес клиента.
-Скрипт должен вывести пользователю:
-Спасибо, ваш заказ будет доставлен по адресу: “тут адрес клиента”
-Номер вашего заказа: #ID
-Это ваш n-й заказ!
-Где ID - уникальный идентификатор только что созданного заказа n - общий кол-во заказов, который сделал
-    пользователь с этим email включая текущий */
-
-$burger = new Burger();
-
-$email = $_POST["email"];
-$name = $_POST["name"];
-$addressField = ["phone", "street", "home", "part", "appt", "floor"];
-$address = "";
-
-foreach ($_POST as $field => $value) {
-    if ($value && in_array($field, $addressField)) {
-        $address .= $value . ",";
-    }
+try {
+    $pdo = new \PDO('mysql:host=localhost;dbname=loftdb;charset=utf8mb4', 'root', '');
+} catch (\Exception $e) {
+    echo $e->getMessage();
 }
-
-$data = ["address" => $address];
-
-$user = $burger->getUserByEmail($email);
-
-if ($user) {
-    $userId = $user["id"];
-    $burger->addCountOrders($user["id"]);
-    $orderCount = $user["orders_count"] + 1;
+$data = [];
+if (empty($_GET['name']) || empty($_GET['email'])) {
+    echo 'Заполните обязательные поля';
 } else {
-    $orderCount = 1;
-    $userId = $burger->createUser($email, $name);
+    $data['name'] = $_GET['name'];
+    $data['email'] = $_GET['email'];
+    $data['phone'] = $_GET['phone'];
+    $data['address'] = $_GET['street'] . ' ' . $_GET['home'] . ' ' . $_GET['part'] . ' ' . $_GET['appt'] . ' ' . $_GET['floor'];
 }
+$ordersCount = 1;
+$createdAt = date('Y-m-d H:i:s');
 
-$orderID = $burger->addOrder($userId, $data);
+$query = "SELECT `email` FROM `users` WHERE `email` = :email";
+$stat = $pdo->prepare($query);
+$stat->execute(['email' => $data['email']]);
+$res = $stat->fetchAll(PDO::FETCH_ASSOC);
 
-echo "Спасибо, ваш заказ будет доставлен по адресу: $address <BR>
-Номер вашего заказа: $orderID <BR>
-Это ваш $orderCount заказ!";
+if ($res) {
+    $address = $data['address'];
+    $ordersID = uniqid();
+   $getNumber=$pdo->prepare('SELECT `orders_count` FROM `users` WHERE `email` = :email');
+   $getNumber->execute(['email' => $data['email']]);
+   $orderNumber=$getNumber->fetchColumn();
+    $query = "INSERT INTO orders(user_id, address, created_at) VALUES (:user_id, :address, :created_at);
+    UPDATE users SET orders_count = orders_count +1 WHERE email = :email";
+    $stat = $pdo->prepare($query);
+    $stat->execute(['user_id' => $ordersID,
+     'address' => $data['address'],
+      'created_at' => $createdAt,
+       'email' => $data['email']]);
+    $stat->fetchAll(PDO::FETCH_ASSOC);
+    echo "Спасибо, ваш заказ будет доставлен по адресу: $address<br>
+Номер вашего заказа: #$ordersID <br>
+Это ваш $orderNumber-й заказ!";
+} else {
+    $ordersID = uniqid();
+    $address = $data['address'];
+    $query = "INSERT INTO users(email, `name`) VALUES (:email, :name);
+    UPDATE users SET orders_count = 1 WHERE email = :email;
+    INSERT INTO orders(user_id, address, created_at) VALUES (:user_id, :address, :created_at)";
+    $stat = $pdo->prepare($query);
+    $stat->execute(['email' => $data['email'],
+     'name' => $data['name'],
+      'user_id' => $ordersID,
+       'address' => $data['address'],
+        'created_at' => $createdAt,
+         'email' => $data['email']]);
+    $stat->fetchAll(PDO::FETCH_ASSOC);
+    echo "Спасибо, ваш заказ будет доставлен по адресу: $address<br>
+Номер вашего заказа: #$ordersID <br>
+Это ваш 1-й заказ!"; 
+}
--- src/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-const DB_HOST = "localhost";
-const DB_NAME = "loftdb";
-const DB_USER = "root";
-const DB_PASSWORD = "";
